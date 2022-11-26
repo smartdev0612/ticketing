fefab89da16cbc462e8219b6d46be11397ca667c
smartdev0612 <danieldev0612@gmail.com>
2022-11-26 10:13:29 -0800

    update client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,7 @@ const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     // We are on the server.
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingres-nginx.svc.cluser.local',
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers
     })
   } else {
--- client/pages/banana.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const banana = () => {
-  return (
-    <div>banana</div>
-  )
-}
-
-export default banana
