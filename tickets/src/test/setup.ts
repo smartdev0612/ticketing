9babd2c0bea2c4c00b17856bd68e7dd15c1383e7
smartdev0612 <danieldev0612@gmail.com>
2022-11-29 19:06:20 -0800

    create tickets signin test

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,9 +2,10 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../app';
+import jwt from 'jsonwebtoken';
 
 declare global {
-  var signin: () => Promise<string[]>
+  var signin: () => string[];
 }
 
 let mongo: any;
@@ -34,14 +35,23 @@ afterAll(async () => {
 
 global.signin = async () => {
   // Build a JWT payload. { id, email }
+  const payload = {
+    id: 'adfdsafd',
+    email: 'test@test.com'
+  }
 
   // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
   // Build session object. { jwt: MY_JWT }
+  const session = { jwt: token };
 
   // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
 
   // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
 
   // return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
 };
