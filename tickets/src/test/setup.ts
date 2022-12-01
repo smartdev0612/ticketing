eae28622fd7a79bb783976f6d32bd361141a1701
smartdev0612 <danieldev0612@gmail.com>
2022-11-30 20:21:20 -0800

    add tickets service in ingress

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,19 +1,19 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose from 'mongoose';
-import request from 'supertest';
-import { app } from '../app';
-import jwt from 'jsonwebtoken';
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../app";
+import jwt from "jsonwebtoken";
 
 declare global {
   var signin: () => string[];
 }
 
 let mongo: any;
-
 beforeAll(async () => {
-  process.env.JWT_KEY = 'asdf';
+  process.env.JWT_KEY = "asdfasdf";
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-  mongo = await MongoMemoryServer.create();
+  const mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
 
   await mongoose.connect(mongoUri, {});
@@ -34,24 +34,24 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  // Build a JWT payload. { id, email }
+global.signin = () => {
+  // Build a JWT payload.  { id, email }
   const payload = {
     id: new mongoose.Types.ObjectId().toHexString(),
-    email: 'test@test.com'
-  }
+    email: "test@test.com",
+  };
 
   // Create the JWT!
   const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  // Build session object. { jwt: MY_JWT }
+  // Build session Object. { jwt: MY_JWT }
   const session = { jwt: token };
 
   // Turn that session into JSON
   const sessionJSON = JSON.stringify(session);
 
   // Take JSON and encode it as base64
-  const base64 = Buffer.from(sessionJSON).toString('base64');
+  const base64 = Buffer.from(sessionJSON).toString("base64");
 
   // return a string thats the cookie with the encoded data
   return [`session=${base64}`];
