4aecab49696d8ca36d319c169a1e5c6f14a577f4
smartdev0612 <danieldev0612@gmail.com>
2022-12-13 02:43:05 -0800

    create unit tests for order service

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -7,6 +7,8 @@ import { Order } from '../models/order';
 
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
 router.post(
   '/api/orders', 
   requireAuth, 
@@ -33,14 +35,22 @@ router.post(
       throw new BadRequestError('Ticket is already reserved');
     }
 
-
     // Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
     // Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket
+    });
+    await order.save();
 
     // Publish an event saying that an order was created
 
-    res.send({});
+    res.status(201).send(order);
   }
 );
 
