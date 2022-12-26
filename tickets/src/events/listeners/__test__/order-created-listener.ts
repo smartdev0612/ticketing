73de26153c65e9367636c7b34b7bd32436418399
smartdev0612 <danieldev0612@gmail.com>
2022-12-26 12:00:50 -0800

    add tickets unit test

--- tickets/src/events/listeners/__test__/order-created-listener.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.ts
@@ -1,4 +1,6 @@
-import { OrderCreatedEvent } from '@daniel0612/common';
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { OrderCreatedEvent, OrderStatus } from '@daniel0612/common';
 import { OrderCreatedListener } from '../order-created-listener';
 import { natsWrapper } from '../../../nats-wrapper';
 import { Ticket } from '../../../models/ticket';
@@ -13,8 +15,25 @@ const setup = async () => {
     price: 99,
     userId: 'asdf'
   });
-  await Ticket.save();
+  await ticket.save();
 
   // Create the fake data event
+  const data: OrderCreatedEvent['data'] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    status: OrderStatus.Created,
+    userId: 'sdfksjl',
+    expiresAt: 'dfsadfl',
+    ticket: {
+      id: ticket.id,
+      price: ticket.price
+    }
+  };
 
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn()
+  };
+
+  return { listener, ticket, data, msg };
 }
